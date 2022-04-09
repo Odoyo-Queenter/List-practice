4d13bffcaade864844f748696a851772199667bb
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-04-09 16:40:36 +0300

    first commit

---
+++ practise.py
@@ -0,0 +1,76 @@
+#what are key features in python
+       #  Coding is Easy: Coding has always been a complex subject, however python proved that coding can be easy. ...
+#Quick Learning Curve: ...
+#Open Source Libraries: ...
+#Support of Community: ...
+#Machine Learning: ...
+#Data Science: ...
+#Automation is Easy: ...
+#Multipurpose:
+
+#Give a diffrence btwm list and tuples
+#list changable
+from audioop import reverse
+import numbers
+
+
+House = ["hut","rentals","apartments","bungalows",156,145.67]
+print(House)
+ 
+
+#tuple unchanagable
+Cars = ("volvo","audi","toyota", 23,45.67)
+print(Cars)
+#explain tertiarry operator in python and how it can be used
+#operators that evaluate something based on a condition being true or false.
+x,y =40,30
+print((x,y) [x>y])
+
+
+#what are negative indices?
+#they are powers with a minus sign infront of them(-1, -3)
+
+#how would you convert a string into a lowercase
+fruits = "APPLE"
+print(fruits.lower())
+
+#how do you get a list of all keys in dictionary
+set1 =(1,2,3,4,5)
+print(set1)
+print(set1[1])
+print([4])
+
+#what is slicing
+#Accessing a range of string characters in  string through their index
+# we comment using #
+
+#how woll you check if all characters in a sting 
+#are alphanumeric
+first_name = "Queenter17"
+print(first_name.isalpha())
+
+second_name ="Achieng"
+print(second_name.isalpha())
+
+#how would you capitalize the first letter in a string
+
+name = "kimberly"
+print(name.upper())
+print(name[0].capitalize())
+print(name.capitalize())
+
+#how do you insert an object at a given index in python
+
+school= "codehive"
+print(school.replace("e","t"))
+
+#how do you reverse a list
+
+numbers = [9,8,7,6,5,4,3,2,1]
+print(numbers.reverse())
+
+#what is a python interpreter prompt
+# its a program installed on your computer named "python3" or "python", and its job is looking at and running your Python code. 
+
+
+
